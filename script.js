501bc0bf424fa1724ded3581a356d00820f36dfb
zaiarias <134075834+zaiarias@users.noreply.github.com>
2023-06-30 04:16:56 -0300

    boton-video-js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function mostrar(){
+    document.getElementById('video').style.display = 'block';
+    }
 document.getElementById("btn-video").onclick = function() {
     document.getElementById("btn-video").remove();
+    mostrar()
 }
+function ocultar(){
+    document.getElementById('video').style.display = 'none';
+    }
+    
+window.onload = ocultar ();
